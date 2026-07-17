c45ac72c0b8811ff3d19f88b5ecad3c2e5f3f973
umer ali <umarmayjan786@gmail.com>
2026-07-16 21:49:49 -0700

    Added dark mode

--- script.js
+++ script.js
@@ -133,3 +133,17 @@ generateBtn.click();
 }
 
 });
+
+const themeBtn = document.getElementById("themeBtn");
+
+themeBtn.addEventListener("click", () => {
+
+    document.body.classList.toggle("dark");
+
+    if(document.body.classList.contains("dark")){
+        themeBtn.innerText = "☀️ Light Mode";
+    }else{
+        themeBtn.innerText = "🌙 Dark Mode";
+    }
+
+});
